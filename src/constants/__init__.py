865df2dfd00248be2bb462082f56a2a99c97c9b6
SDprogramer <soumyadeep.dey162003@gmail.com>
2024-12-05 13:23:35 +0530

    Added

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,4 +1,4 @@
-MONGODB_URL_KEY: str = "MONGO_DB_URL"
+MONGODB_URL_KEY: str = "mongodb+srv://soumyadeep162003:s12345@cluster0.jp3ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 MONGO_DATABASE_NAME: str = "myntra-reviews"
 
 SESSION_PRODUCT_KEY: str = "product_name"
